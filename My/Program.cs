d726a5d923451175e218ff0d5aedfc7eddb9e212
Urieva Irina <io77s@yandex.ru>
2018-12-12 16:48:17 +0300

    1

---
+++ My/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Знакомство с GitHub");
+            //Console.WriteLine("Проверка изменений на сервере");
+           // Console.WriteLine("Проверка ");
+        }
+    }
+    }
+}
